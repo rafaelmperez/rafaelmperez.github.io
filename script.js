fbd46c53f84d43e7a5058c39521876ef38b3a64e
rafaelmperez <theworldoffragrance@gmail.com>
2025-08-30 01:09:32 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const sound = document.getElementById("menu-sound");
-const links = document.querySelectorAll("nav a");
-
-links.forEach(link => {
-  link.addEventListener("mouseenter", () => {
-    sound.currentTime = 0; 
-    sound.play();
-  });
-});
