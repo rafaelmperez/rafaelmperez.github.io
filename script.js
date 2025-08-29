ce9ce8be56b0adff3549d2164f13ee58d30799de
rafaelmperez <theworldoffragrance@gmail.com>
2025-08-30 01:15:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const sound = document.getElementById("menu-sound");
-const links = document.querySelectorAll("nav a");
+document.addEventListener("DOMContentLoaded", () => {
+  const sound = document.getElementById("menu-sound");
+  const links = document.querySelectorAll("nav a");
 
-links.forEach(link => {
-  link.addEventListener("mouseenter", () => {
-    sound.currentTime = 0; 
-    sound.play();
+  links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+      sound.currentTime = 0; 
+      sound.play();
+    });
   });
 });
