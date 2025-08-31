6edf5aa9c74def0eee5b7388c7a485ae9ba67365
rafaelmperez <theworldoffragrance@gmail.com>
2025-08-31 16:42:30 +0200

    Update scriptlectura.js

--- scriptlectura.js
+++ scriptlectura.js
@@ -5,19 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', () => {
       document.body.classList.toggle('reading-mode');
 
-      // Cambia el texto del botón según el estado
+      // Cambia el texto del botón
       btn.textContent = document.body.classList.contains('reading-mode')
         ? '⛔ Salir del modo lectura'
         : '🕮 Modo lectura';
-
-      // Guarda preferencia en localStorage
-      localStorage.setItem('modoLectura', document.body.classList.contains('reading-mode'));
     });
-
-    // Aplica modo lectura si estaba activado previamente
-    if (localStorage.getItem('modoLectura') === 'true') {
-      document.body.classList.add('reading-mode');
-      btn.textContent = '⛔ Salir del modo lectura';
-    }
   }
 });
