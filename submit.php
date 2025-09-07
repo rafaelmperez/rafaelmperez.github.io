97bc811433aaa2da6b5cae150c87d2024ae98fa8
rafaelmperez <theworldoffragrance@gmail.com>
2025-09-07 13:56:41 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,5 +1,7 @@
 <?php
-$nombre = $_POST['nombre'];
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$mensaje = $_POST['message'];
 $captcha = $_POST['g-recaptcha-response'];
 
 if (!$captcha) {
@@ -14,7 +16,7 @@ $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?sec
 $response = json_decode($verify);
 
 if ($response->success) {
-  echo "Formulario enviado correctamente. Hola, " . htmlspecialchars($nombre);
+  echo "¡Formulario enviado correctamente! Gracias, " . htmlspecialchars($nombre);
   // Aquí puedes guardar los datos, enviar email, etc.
 } else {
   echo "Verificación fallida. Intenta de nuevo.";
